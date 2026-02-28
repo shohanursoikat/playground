846d66e78822fe0a0483f80ac766441cf220d0b1
Shohanur Rahman Soikat <shohanoursam@gmail.com>
2026-03-01 03:55:15 +0600

    updated

---
+++ Python/quicksort.py
@@ -0,0 +1,20 @@
+arr = [10, 7, 8, 9, 1, 5]
+def quick_sort(arr):
+    if len(arr) <= 1:  # Base case
+        return arr
+
+    pivot = arr[0]
+
+    left = []
+    right = []
+
+    for x in arr[1:]:
+        if x <= pivot:
+            left.append(x)
+        else:
+            right.append(x)
+
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
+
+print(quick_sort(arr))
